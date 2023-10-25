3aee78917f00f699f701944489bdd94c17b0bd5f
Thiago Brito <britotr@gmail.com>
2023-10-25 15:45:49 -0300

    intro ao JS

---
+++ aula01.js
@@ -0,0 +1,37 @@
+// let nomeCompleto; // camel case
+
+// let NomeCompleto; //pascal case
+
+// let nome_completo; // snake case
+
+// let NOME_COMPLETO; // upper snake case
+
+// const API_DEV = 'https://10.20.33.0/api/v2';
+
+// let age = 10;
+
+// let dinheiro = 10.30
+
+// let nomeCadastroPessoaFisica = 'Thiago Brito'
+
+// let isNomeValidoFormularioCadastroPessoaFisica = true
+
+// let linguagens = ['Javascript', 'Java', 'Python', 10, [1, 2, 3, 4], {id: 1, nome: 'Jose'}, 1, 2, '', null]
+
+// let pessoa = {
+//   nome: 'Thiago', 
+//   idade: 19, 
+//   profissao: 'Professor',
+//   isCasado: true
+// }
+
+// console.log(pessoa.idade)
+
+let idade = 1000;
+let numero = '500'
+
+// console.log(idade.toString() + 10)
+// console.log(String(idade) + 10)
+// console.log(idade + 10)
+
+console.log(typeof numero === 'string')
